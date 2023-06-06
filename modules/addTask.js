fcb99d40619ae3217dafcd0858088fb62cfe4a7a
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-06 14:30:16 -0500

    Change the position of the elements

--- modules/addTask.js
+++ modules/addTask.js
@@ -5,8 +5,8 @@ export default function addTask() {
   /* Create a new instance of the Task Class */
   const task = new TaskConstructor(taskInput.value);
 
-  task.createTask();
   task.storage();
+  task.createTask(task);
   /* Eliminates the information in the input */
   taskInput.value = '';
 }
