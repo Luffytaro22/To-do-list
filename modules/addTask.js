c8f4002e06430e6946f245230cd0290fbd19e5e8
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-06 13:48:34 -0500

    Import the TaskConstructor class

--- modules/addTask.js
+++ modules/addTask.js
@@ -1,12 +1,12 @@
-import TaskConstructor from "./constructor";
-import { taskInput } from "./elements";
+import TaskConstructor from './constructor';
+import { taskInput } from './elements';
 
 export default function addTask() {
   /* Create a new instance of the Task Class */
   const task = new TaskConstructor(taskInput.value);
 
   task.createTask();
-
+  task.storage();
   /* Eliminates the information in the input */
   taskInput.value = '';
 }
