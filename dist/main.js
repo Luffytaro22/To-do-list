5cef625f20e7832b199527e83a22dc068f62dca4
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-06 11:53:02 -0500

    Added webpacks

---
+++ dist/main.js
@@ -0,0 +1,18 @@
+(self["webpackChunkto_do_list"] = self["webpackChunkto_do_list"] || []).push([["main"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
