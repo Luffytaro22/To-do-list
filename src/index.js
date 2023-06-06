af3d2f54161f68d3ca57977499353d9e16028c32
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-06 12:45:49 -0500

    Added an event listener to the input

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import './styles.css'
 
+import { taskInput, clearButton } from '../modules/elements.js';
+import addTask from '../modules/addTask.js';
+
+taskInput.addEventListener('keypress', (event) => {
+  /* If the code of the keyword is 13 (enter) then calls the function */
+  if (event.charCode === 13) {
+    addTask();
+  }
+});
