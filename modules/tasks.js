db50f3dbf45c3d8694ce050289efd90348baa8ac
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-06 12:22:27 -0500

    Created a Task class

--- modules/tasks.js
+++ modules/tasks.js
@@ -1,4 +1,4 @@
-import { listContainer } from './elements.js';
+import { tasksContainer } from './elements.js';
 
 let tasks = [
   {
@@ -7,3 +7,29 @@ let tasks = [
     index: 1,
   }
 ];
+
+class Task {
+  constructor(description) {
+    this.description = description;
+  }
+
+  createTask() {
+    /* Create elements */
+    const li = document.createElement('li');
+    const checkbox = document.createElement('input');
+    const pDescription = document.createElement('p');
+    const dotsIcon = document.createElement('i');
+
+    /* Assign classes and attributes */
+    checkbox.type = 'checkbox';
+    checkbox.value = 'off';
+    pDescription.textContent = this.description;
+    dotsIcon.classList.add('fa-solid', 'fa-ellipsis-vertical');
+
+    /* Append elements */
+    li.appendChild(checkbox);
+    li.appendChild(pDescription);
+    li.appendChild(dotsIcon);
+    tasksContainer.appendChild(li);
+  }
+}
