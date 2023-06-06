a3293b57b9292302d56eb4dd55755250d639eba4
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-06 14:30:44 -0500

    Import the task class and created a instance to display the added tasks

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import './styles.css'
 
 import { taskInput, clearButton } from '../modules/elements.js';
 import addTask from '../modules/addTask.js';
+import { Task } from '../modules/tasks';
+
+const myTasks = new Task();
+myTasks.displayTasks();
 
 taskInput.addEventListener('keypress', (event) => {
   /* If the code of the keyword is 13 (enter) then calls the function */
