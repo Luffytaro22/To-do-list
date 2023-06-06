2da721cbb5f946601568715ef9745ad0a9dae5f7
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-06 12:31:12 -0500

    Added the button element

--- modules/elements.js
+++ modules/elements.js
@@ -1,5 +1,6 @@
 const listContainer = document.querySelector('#list-container');
 const tasksContainer = document.getElementById('tasks-container');
 const taskInput = document.getElementById('task');
+const clearButton = document.getElementById('clear-button');
 
-export {listContainer, tasksContainer, taskInput};
+export {listContainer, tasksContainer, taskInput, clearButton};
