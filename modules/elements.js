ae4fa917f7063cbdc67521d27712fe53774c8b5e
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-08 14:09:44 -0500

    Added the checkboxs

--- modules/elements.js
+++ modules/elements.js
@@ -3,8 +3,9 @@ const tasksContainer = document.getElementById('tasks-container');
 const taskInput = document.getElementById('task');
 const clearButton = document.getElementById('clear-button');
 const enterIcon = document.getElementById('enter-icon');
+const checks = document.querySelectorAll('input[type="checkbox"]')
 
 export {
   listContainer, tasksContainer, taskInput, clearButton,
-  enterIcon,
+  enterIcon, checks,
 };
