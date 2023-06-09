bfe88fa0645f28f163bf0e7d7cd08e52479c2aad
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-08 22:35:55 -0500

    Add the animation when the dark mode icon is clicked

--- modules/dark-mode.js
+++ modules/dark-mode.js
@@ -1,5 +1,6 @@
 const all= document.documentElement;
-
+import { darkModeIcon } from "./elements.js";
 export default function darkMode() {
   all.classList.toggle('dark-mode');
+  darkModeIcon.classList.toggle('animation');
 }
