dbebf301a9498c507775859c79d7a02086893823
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-10 11:52:01 -0500

    Add the web icon to a link element

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import addTask from '../modules/addTask.js';
 import Task from '../modules/tasks.js';
 import Clear from '../modules/clear-button.js';
 import { handleDragStart, handleDragOver, handleDrop } from '../modules/drag.js';
+import webIcon from '../src/assets/lista-de-deseos.png';
 
 const dotsIcons = document.querySelectorAll('.fa-ellipsis-vertical');
 const myTasks = new Task();
@@ -14,6 +15,11 @@ myTasks.displayTasks();
 
 const newClear = new Clear();
 
+/* Creates a new link element and added it to the head */
+const Image = document.createElement('link');
+Image.href = webIcon;
+Image.rel = 'icon';
+document.querySelector('head').appendChild(Image);
 taskInput.addEventListener('keypress', (event) => {
   /* If the code of the keyword is 13 (enter) then calls the function */
   if (event.charCode === 13 && taskInput.value !== '') {
