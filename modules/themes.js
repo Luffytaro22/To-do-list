3e830ddc6de0e4f7bd734ece9d632f5e6a1b8738
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-11 14:59:21 -0500

    Added the functionality for the yellow theme

--- modules/themes.js
+++ modules/themes.js
@@ -4,6 +4,11 @@ export default function themeMode(event) {
   let themeStatus = { status: event.target.value };
   localStorage.setItem('darkMode', JSON.stringify(themeStatus));
   if (event.target.value === 'Rose') {
+    root.classList.remove('yellow-mode');
     root.classList.add('rose-mode');
+  }
+  if (event.target.value === 'Yellow') {
+    root.classList.remove('rose-mode');
+    root.classList.add('yellow-mode');
   }
 }
