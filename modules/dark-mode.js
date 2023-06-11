047a87e246a7e9c59455be8e65dc8ab80e483edb
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-10 21:09:35 -0500

    Added a object to save the information of the theme in the local storage

--- modules/dark-mode.js
+++ modules/dark-mode.js
@@ -1,7 +1,32 @@
 import { darkModeIcon } from './elements.js';
 
 const all = document.documentElement;
-export default function darkMode() {
+
+function darkMode() {
+  let darkStatus = localStorage.getItem('darkMode') ? JSON.parse(localStorage.getItem('darkMode')) : { status: 'day', };
   all.classList.toggle('dark-mode');
   darkModeIcon.classList.toggle('animation');
+  if (darkStatus.status === 'day') {
+    /* Change the status to night and update the local storage */
+    darkStatus.status = 'night';
+    localStorage.setItem('darkMode', JSON.stringify(darkStatus));
+  } else {
+    /* Change the status to night and update the local storage */
+    darkStatus.status = 'day';
+    localStorage.setItem('darkMode', JSON.stringify(darkStatus));
+  }
 }
+
+function setTheme() {
+  /* Initialize the object with the info in the local storage */
+  let darkStatus = localStorage.getItem('darkMode') ? JSON.parse(localStorage.getItem('darkMode')) : { status: 'day', };
+  if (darkStatus.status === 'day') {
+    all.classList.remove('dark-mode');
+    darkModeIcon.classList.remove('animation');
+  } else {
+    all.classList.add('dark-mode');
+    darkModeIcon.classList.add('animation');
+  }
+}
+
+export { darkMode, setTheme, };
