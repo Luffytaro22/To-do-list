d4018d6c6c4f8ca7850645af9774c8e6a7b7aea5
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-10 21:24:13 -0500

    Added a function to charge the saved font of the local storage

--- modules/fonts.js
+++ modules/fonts.js
@@ -1,5 +1,15 @@
 import { root } from './elements.js';
 
-export default function changeFont(event) {
+function changeFont(event) {
+  let font = event.target.value;
   root.style.setProperty('--main-font', event.target.value);
+  /* Update the local storage */
+  localStorage.setItem('font', JSON.stringify(font));
 }
+
+function setFont() {
+  let font = localStorage.getItem('font') ? JSON.parse(localStorage.getItem('font')) : 'yuji hentaigana akebono';
+  root.style.setProperty('--main-font', font);
+}
+
+export { changeFont, setFont, };
