1daf3a0bf66b75fa3d59bee9965571eeb5c7ee63
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-06-14 22:33:44 -0500

    Modified the elements

--- src/index.test.js
+++ src/index.test.js
@@ -4,20 +4,28 @@
 
 import TaskConstructor from '../modules/constructor.js';
 
-jest.mock('../modules/tasks.js');
 
 describe('adding tasks', () => {
   test('Add one new item to the list', () => {
     document.body.innerHTML =
-      `<ul id="tasks-container"> 
-          <li></li>
+      `<ul id="tasks-container">
+          <input type="text" id="task"><i class="fa-solid fa-arrow-right-to-bracket" id="enter-icon"></i>
           <button type="button" id="clear-button"></button>
       </ul>`
 
-    const task = new TaskConstructor('add');
-    task.createTask(task);
+    require('../modules/tasks.js');
+    //require('./index.js');
+
+    const tasksContainer = document.getElementById('tasks-container');
+    const input = document.getElementById('task');
+    const clearButton = document.getElementById('clear-button');
+    const enterIcon = document.getElementById('enter-icon');
+
+    input.value = 'add';
+    enterIcon.click();
 
     const list = document.querySelectorAll('#tasks-container li');
+    const list1 = document.querySelector('#tasks-container li:first-child');
     expect(list).toHaveLength(1);
   });
-})
+});
